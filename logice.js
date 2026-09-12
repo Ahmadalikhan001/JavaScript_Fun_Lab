02091f98142655fb75f8b47bea6085c61b0306ad
Ahmad Ali Khan <ahmadalik282@gmail.com>
2026-09-12 16:10:06 +0800

    Starting the JS

---
+++ logice.js
@@ -0,0 +1,10 @@
+console.log("Hello, World!");
+let x = 5;
+let y = 10;
+console.log("The sum of x and y is : ", x + y);
+let z = "This is a string variable.";
+console.log(typeof x, typeof y, typeof z);
+
+for (let i = 0; i <= 5; i++) {
+    console.log("The value of i is: ", i);
+}
