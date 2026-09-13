2a65123dbbe5cc68f38be1ea8bd8561ec79b2a2a
Ahmad Ali Khan <ahmadalik282@gmail.com>
2026-09-13 12:35:42 +0800

    Practicing the for loop

--- logice.js
+++ logice.js
@@ -1,10 +1,19 @@
-console.log("Hello, World!");
-let x = 5;
-let y = 10;
-console.log("The sum of x and y is : ", x + y);
-let z = "This is a string variable.";
-console.log(typeof x, typeof y, typeof z);
+// console.log("Hello, World!");
+// let x = 5;
+// let y = 10;
+// console.log("The sum of x and y is : ", x + y);
+// let z = "This is a string variable.";
+// console.log(typeof x, typeof y, typeof z);
 
-for (let i = 0; i <= 5; i++) {
-    console.log("The value of i is: ", i);
+// for (let i = 0; i <= 5; i++) {
+//     console.log("The value of i is: ", i);
+// }
+
+let a = 12;
+let b = 23;
+console.log(b + a);
+
+let c = 0;
+for (c = 0; c <= 10; c++) {
+    console.log("The value of c is: ", c);
 }
