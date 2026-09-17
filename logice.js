d215c88a9366d23961aa917473024d66bf31d6ef
Ahmad Ali Khan <ahmadalik282@gmail.com>
2026-09-17 14:55:48 +0800

    ! Logical Operatos

--- logice.js
+++ logice.js
@@ -168,10 +168,18 @@
 //     console.log("You can enter");
 // }
 // its print "You can enter" if the age is greater than or equal to 18 and hasID is true.
-let age = 16;
-let hasParent = true;
+// let age = 16;
+// let hasParent = true;
 
-if (age >= 18 || hasParent === true) {
-    console.log("You can enter");
-}
+// if (age >= 18 || hasParent === true) {
+//     console.log("You can enter");
+// }
 // its print "You can enter" if the age is greater than or equal to 18 or hasParent is true. so the output will be "You can enter" because the age is less than 18 but hasParent is true.
+
+let hasID = false;
+
+if (!hasID) {
+    console.log("You don't have an ID");
+}
+
+// its print "You don't have an ID" if hasID is false. so the output will be "You don't have an ID" because hasID is false.
