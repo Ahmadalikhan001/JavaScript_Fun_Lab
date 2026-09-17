09bf00f73d75d55944e29b497159f153d8217258
Ahmad Ali Khan <ahmadalik282@gmail.com>
2026-09-17 17:38:12 +0800

    Grade Calculator using Ternary operator more improve Versio

--- logice.js
+++ logice.js
@@ -301,6 +301,10 @@
 
 // using the Ternary operator
 
-let marks = 40;
-let graid = (marks >= 80 ? "A+" : marks >= 60 && marks < 80 ? "B" : marks >= 50 && marks < 60 ? "C" : "Fail");
-console.log(graid);
+// let marks = 40;
+// let graid = (marks >= 80 ? "A+" : marks >= 60 && marks < 80 ? "B" : marks >= 50 && marks < 60 ? "C" : "Fail");
+// console.log(graid);
+
+let marks = 70;
+let result = (marks >= 80 ? "A+" : marks >= 70 && marks < 80 ? "A" : marks >= 60 && marks < 70 ? "B" : marks >= 50 && marks < 60 ? "C" : marks >= 50 && marks < 60 ? "C" : "Fail");
+console.log(result);
