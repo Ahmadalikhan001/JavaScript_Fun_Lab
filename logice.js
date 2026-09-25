7f65c4fba1db16d753f6af8988c46b174793b344
Ahmad Ali Khan <ahmadalik282@gmail.com>
2026-09-25 14:49:37 +0800

    .find () method

--- logice.js
+++ logice.js
@@ -850,9 +850,16 @@ const Add = (a, b) => a + b;
 // console.log(student4);
 
 // filter() → select some items
-let student5 = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 0, 1, 21, 2, 22, 33, 44, 66];
-let student6 = student5.filter((item) => {
-    return item % 2 === 0;
-})
-console.log(student6);
-console.log(student5);
+// let student5 = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 0, 1, 21, 2, 22, 33, 44, 66];
+// let student6 = student5.filter((item) => {
+//     return item % 2 === 0;
+// })
+// console.log(student6);
+// console.log(student5);
+
+// find() → find ONE item
+
+let Num = [1, 3, 3, 2, 4, 5, 6, 7, 7];
+
+let filterNum = Num.find(item => item > 2);
+console.log(filterNum);
